3997180ae67a4c1f0e710c8b4569141314952289
santoshsharma27 <santosh37kr@gmail.com>
2025-12-27 19:49:45 +0530

    completed project 2

---
+++ routing-rendering/app/(content)/news/[slug]/@modal/(.)image/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { notFound, useRouter } from "next/navigation";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+  const { slug } = React.use(params);
+
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" onClick={() => router.back()} />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
---
+++ routing-rendering/app/(content)/news/[slug]/page.jsx
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default async function NewsDetailPage({ params }) {
+  const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <article className="news-article">
+      <header>
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
+}
